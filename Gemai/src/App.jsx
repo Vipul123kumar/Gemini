e9882b92ecc9a559678de1662c87c5ac870f6ad7
Vipul Singh <dhekcha628@gmail.com>
2025-06-09 19:25:13 +0530

    first commit

---
+++ Gemai/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Chatbot from './components/Sidebar/Chatbot'
+
+const App = () => {
+  return (
+    <>
+      {/* <Sidebar/>
+    <Main/> */}
+    <Chatbot/>
+    </>
+  )
+}
+
+export default App
+
